e013cddeffae8e060eb9bea46ea8cec7895a0567
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2025-07-14 18:13:44 +0530

    create note api done

---
+++ backend/src/modules/note/note.controller.js
@@ -0,0 +1,47 @@
+import Notes from "./note.model.js";
+import createError from 'http-errors'
+// create Notes 
+const createNotes = async (req, res, next) => {
+  try {
+    const { title, content, isPinned, isArchived, isStarred, attachments, tags } = req.body;
+    if (!title) {
+      const err = createError(400, "Title is required!");
+      return next(err);
+    }
+    if (!content) {
+      const err = createError(400, "Content is required!");
+      return next(err);
+    }
+    const userId = req.userId;
+    const newNotesObject = {
+      user: userId,
+      title: title,
+      content: content,
+      ...(Array.isArray(tags) ? { tags } : {}),
+      ...(typeof isPinned === "boolean" ? { isPinned } : {}),
+      ...(typeof isArchived === "boolean" ? { isArchived } : {}),
+      ...(typeof isStarred === "boolean" ? { isStarred } : {}),
+      ...(Array.isArray(attachments) ? { attachments } : {}),
+    };
+
+    const newNotes = await Notes.create(newNotesObject);
+    await newNotes.save();
+
+    // toObject() => convert mongodb document to javascript object 
+    const notesResponse = newNotes.toObject();
+    delete notesResponse.user; // remove the user field from notes object
+
+    return res.status(200).json({
+      success: true,
+      message: "Notes created successFully!",
+      notes: notesResponse
+    })
+  } catch (error) {
+    // error ko next middleware ko pass karo
+    return next(error);
+  }
+}
+
+
+export { createNotes }
+
---
+++ backend/src/modules/note/note.model.js
@@ -0,0 +1,42 @@
+import mongoose from 'mongoose'
+
+const noteSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+    required: true
+  },
+  tags: {
+    type: [String],
+    default: []
+  },
+  isPinned: {
+    type: Boolean,
+    default: false
+  },
+  isArchived: {
+    type: Boolean,
+    default: false,
+  },
+  isStarred: {
+    type: Boolean,
+    default: false,
+  },
+  attachments: {
+    type: [String],
+    default: []
+  },
+})
+
+const Notes = mongoose.model("Note", noteSchema);
+export default Notes;
+
+
