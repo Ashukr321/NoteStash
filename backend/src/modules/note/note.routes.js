0a51b56dc5ce3f7a3d78e88333582cdccf5b2595
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2025-07-14 19:13:54 +0530

    notes delete api done

--- backend/src/modules/note/note.routes.js
+++ backend/src/modules/note/note.routes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import {createNotes,getAllNotes,getNoteById} from './note.controller.js'
+import {createNotes,getAllNotes,getNoteById,deleteNote} from './note.controller.js'
 import isAuthenticate from '../../middlewares/authMiddleware.js';
 
 // create router 
@@ -8,6 +8,7 @@ const router = express.Router();
 router.post('/',isAuthenticate,createNotes);
 router.get('/',isAuthenticate,getAllNotes);
 router.get('/:id',isAuthenticate,getNoteById);
+router.delete('/:id',isAuthenticate,deleteNote);
 
 // export routes
 export default router;
